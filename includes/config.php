8edbf829865133fe1bda3ec869555ad76c1d70df
Polina Zhilyaeva <zhilyaeva.polina@gmail.com>
2017-08-14 21:39:54 +0300

    Fixed db user

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define ( 'DB_HOST', 'eu-cdbr-west-01.cleardb.com' );
-define ( 'DB_USER', 'zbf5a0ae685c06a' );
+define ( 'DB_USER', 'bf5a0ae685c06a' );
 define ( 'DB_PASSWORD', 'b36037ec' );
 define ( 'DB_NAME', 'heroku_0ba6140862f5f76' );
