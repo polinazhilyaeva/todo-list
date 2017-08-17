7053c0e9875d6375146376d913558446d610f57c
Polina Zhilyaeva <zhilyaeva.polina@gmail.com>
2017-08-17 18:38:25 +0300

    Added config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@ define ( 'DB_PASSWORD', 'wp' );
 define ( 'DB_NAME', 'todo-app' );
 
 
-// Production (Heroku) credentials for this app
+// Production credentials for this app
 
 /*define ( 'DB_HOST', 'eu-cdbr-west-01.cleardb.com' );
 define ( 'DB_USER', 'bf5a0ae685c06a' );
