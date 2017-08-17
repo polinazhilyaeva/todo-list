918fecd004d8381d79bdf81a429aebd25dac49d2
Polina Zhilyaeva <zhilyaeva.polina@gmail.com>
2017-08-17 18:35:06 +0300

    gitignore

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Development credentials for this app
+
+define ( 'DB_HOST', 'localhost' );
+define ( 'DB_USER', 'wp' );
+define ( 'DB_PASSWORD', 'wp' );
+define ( 'DB_NAME', 'todo-app' );
+
+
+// Production (Heroku) credentials for this app
+
+/*define ( 'DB_HOST', 'eu-cdbr-west-01.cleardb.com' );
+define ( 'DB_USER', 'bf5a0ae685c06a' );
+define ( 'DB_PASSWORD', 'b36037ec' );
+define ( 'DB_NAME', 'heroku_0ba6140862f5f76' );*/
+
+// Timezone
+
+define('TIMEZONE', 'Europe/Kiev');
