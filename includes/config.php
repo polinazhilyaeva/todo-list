84ab4cb3e300643c5254b9f95c89539c295a75c5
Polina Zhilyaeva <zhilyaeva.polina@gmail.com>
2017-08-17 18:40:06 +0300

    Added config.php

--- includes/config.php
+++ includes/config.php
@@ -1,19 +1,11 @@
 <?php
 
-// Development credentials for this app
+// Production database credentials
 
-define ( 'DB_HOST', 'localhost' );
-define ( 'DB_USER', 'wp' );
-define ( 'DB_PASSWORD', 'wp' );
-define ( 'DB_NAME', 'todo-app' );
-
-
-// Production credentials for this app
-
-/*define ( 'DB_HOST', 'eu-cdbr-west-01.cleardb.com' );
+define ( 'DB_HOST', 'eu-cdbr-west-01.cleardb.com' );
 define ( 'DB_USER', 'bf5a0ae685c06a' );
 define ( 'DB_PASSWORD', 'b36037ec' );
-define ( 'DB_NAME', 'heroku_0ba6140862f5f76' );*/
+define ( 'DB_NAME', 'heroku_0ba6140862f5f76' );
 
 // Timezone
 
